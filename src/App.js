eb88b78f06fe4165c3400926b7b7271f3293565f
Talal Malick <m.talalmalick2014@gmail.com>
2019-11-16 01:44:50 +0500

    fix route issue

--- src/App.js
+++ src/App.js
@@ -1,53 +1,39 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import PropTypes from "prop-types";
 import routes, { notAuthenticatedRoutes } from "./routes";
 import withTracker from "./withTracker";
-import Store from "./flux/store"
+import Store from "./flux/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
 import Signin from "./views/Signin";
-
+import BasicDetailsSTP from "./views/BasicDetailsSTP";
+import Signup from "./views/Signup";
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isAuth: Store.getloginstatus() };
-  }
-  componentDidMount(){
-    console.log(Store.getloginstatus())
-  }
-  
   render() {
     return (
       <Router basename={process.env.REACT_APP_BASENAME || ""}>
-        <div>
-          {!Store.getloginstatus() &&
-            notAuthenticatedRoutes.map((route, index) => {
-              return (
-                <React.Fragment key={index}>
-                  <Route exact path={route.path} component={route.component} />
-                </React.Fragment>
-              );
-            })}
-          {Store.getloginstatus() &&
-            routes.map((route, index) => {
-              return (
-                <Route
-                  key={index}
-                  path={route.path}
-                  exact={route.exact}
-                  component={withTracker(props => {
-                    return (
-                      <route.layout {...props}>
-                    
-                        <route.component {...props} />
-                      </route.layout>
-                    );
-                  })}
-                />
-              );
-            })}
-        </div>
+        <Switch>
+          <Route exact path="/" component={Signin} />
+          <Route exact path="/Signup" component={Signup} />
+          <Route exact path="/Details" component={BasicDetailsSTP} />
+          {routes.map((route, index) => {
+            return (
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={withTracker(props => {
+                  return (
+                    <route.layout {...props}>
+                      <route.component {...props} />
+                    </route.layout>
+                  );
+                })}
+              />
+            );
+          })}
+        </Switch>
       </Router>
     );
   }
